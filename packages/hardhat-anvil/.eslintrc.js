afda6a4ddd17a44eef0bf32402b85f2721ab2dd0
sam bacha <sam@manifoldfinance.com>
2022-05-22 03:17:22 -0700

    lint(fix): eslint

--- packages/hardhat-anvil/.eslintrc.js
+++ packages/hardhat-anvil/.eslintrc.js
@@ -1,7 +1,41 @@
 module.exports = {
-  extends: [`${__dirname}/../../config/eslint/eslintrc.js`],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-    sourceType: "module",
+    ecmaVersion: 12, // es2021 sets the ecmaVersion parser option to 12.
+    sourceType: 'module',
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'max-len': [2, 130, 4],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'no-plusplus': 0,
+    'no-underscore-dangle': 0,
+    'prefer-rest-params': 0,
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
   },
 };
